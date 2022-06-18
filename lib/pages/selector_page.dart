3aa66b3bfcf8e91e92d9f0c1b5c6cc9a16d54e51
Natalia Shishuk <natalia.shishuk@gmail.com>
2022-06-18 15:02:25 +0300

    Create empty pages

---
+++ lib/pages/selector_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SelectorPage extends StatelessWidget {
+  const SelectorPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
